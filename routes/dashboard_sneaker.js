e17f77af56c79e972dbbe83d5da99629fed19d38
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-02-06 17:38:09 +0100

    tried working on tag form

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,7 +1,8 @@
 const express = require("express"); // import express in this module
 const router = new express.Router(); // create an app sub-module (router)
 const uploadCloud = require('../config/cloudinary');
-const sneakerModel = require('../models/Sneaker')
+const sneakerModel = require('../models/Sneaker');
+const tagModel = require('../models/Tag');
 
 router.get("/prod-add", (req,res) =>{
     res.render("products_add");
@@ -19,5 +20,15 @@ router.post("/prod-add", uploadCloud.single('photo'), (req,res,next) =>{
     })
     .catch(next);
 })
+
+// **** ADD TAG ROUTE FOR AJAX ****
+
+// router.post("/prod-add", (req, res, next) => {
+//     const {label} = req.body;
+//     tagModel
+//     .create()
+//     .then(() => res.redirect("/sec/prod-add"))
+//     .catch(next);
+// })
 
 module.exports = router;
