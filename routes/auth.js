81787c252d0609a6cf8352615cd376fe1220f08c
Remy Pham <remy.8P@gmail.com>
2020-02-06 12:58:01 +0100

    signin/up views done

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,22 @@
 const express = require("express");
 const router = new express.Router();
+const userModel = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+
+// views form signin/signup
+router.get("/signin", (req, res) => {
+    res.render("signin");
+});
+
+router.get("/signup", (req, res) => {
+    res.render("signup");
+});
+
+
+// CREATE ACCOUNT SIGNUP
+
+
+
+
 module.exports = router;
